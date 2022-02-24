2f34c4e95734aadb71572e1e1add9bb333222a17
abiryusuf <yusuf_ctg@yahoo.com>
2022-02-23 20:14:23 -0500

    tuple

--- 01_Data_Structure/001_Tuple.py
+++ 01_Data_Structure/001_Tuple.py
@@ -2,3 +2,18 @@
 Tuple are used to store multiple items in a single variable
 A tuple is a collection which is ordered and unchangeable
 """
+x = (1,)
+print (x)
+
+a = ("Abir", 27, "NY")  # packing
+(name, age, place) = a  # unpacking
+print(name, age, place)
+
+# tuple comparison starts with a first element of each tuple
+x = (5, 6)
+y = (6, 8)
+
+if x > y:
+    print("X is bigger")
+else:
+    print("Y is bigger")
