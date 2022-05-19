4c4b3458acac7edd71698029be807daa2e8d4760
abiryusuf <yusuf_ctg@yahoo.com>
2022-05-18 20:51:42 -0400

    reviwe the python

---
+++ w3_School/Print.py
@@ -0,0 +1,13 @@
+# How to print blank lines
+# Sometimes we need to print one blank line
+
+# \n divided or space the line
+print ("Welcome to python \n" )
+#print("\n")
+print("Abir")
+
+# end="" add the line
+print("Welcome to", end=' ')
+print("Python", end='!' ' ')
+print("abir", end='')
+print("c")
