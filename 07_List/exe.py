9200f58a8b57257f561592fb6a8d81ec97a0650d
abiryusuf <yusuf_ctg@yahoo.com>
2022-07-13 18:43:22 -0400

    tuple

--- 07_List/exe.py
+++ 07_List/exe.py
@@ -2,4 +2,24 @@
 # List items are ordered, changeable, and allow duplicate values"""
 
 x = [4, 6, 7, 8]
-print(x[1:3])
+# x.append(9)
+# x.insert(1, 10)
+# x.pop(0)
+# del x[1]
+# y = len(x)
+# for i in range(y):
+#     print(i, end=" ")
+
+newList = []
+for i in x:
+    if i < 10:
+        newList.append(i)
+print(newList)
+
+for i in range(1, 10):
+    newList.append(i * 2)
+print(newList)
+
+listCom = [i * 2 for i in range(1, 10)]
+print(listCom)
+
---
+++ 08_Tuple/Demo.py
@@ -0,0 +1,9 @@
+"""
+Tuple is a collection which is ordered and unchangeable
+and tuples are written with round brackets.
+
+Tuples are unchangeable, meaning that we cannot change, add or remove items after the
+tuple has been created
+"""
+thisTuple = ("Apple",)  # need a comma for a tuple or it will be string
+print(type(thisTuple))
