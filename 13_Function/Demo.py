f05e6feb31f0afa9e6f957aba2911dbc69fac45c
abiryusuf <yusuf_ctg@yahoo.com>
2022-08-03 22:34:40 -0400

    function

--- 13_Function/Demo.py
+++ 13_Function/Demo.py
@@ -1,9 +1,32 @@
 
+"""
+Function is a block of codes that will run when we call it
+we can pass the data in the parameters into a function
+"""
+# def fac(n):
+#     s = 1
+#     for i in range(1, n):
+#         s += i * 2
+#     print(s)
+#
+# fac(5)
 
-def fac(n):
-    s = 1
-    for i in range(1, n):
-        s += i * 2
-    print(s)
+def myFunction(x, y):
+    return "First Name " + x + " Last Name " + y
 
-fac(5)
+print(myFunction("Abir", "Yusuf"))
+
+def my_function(**name):
+    print("First Name " + name["lname"])
+
+my_function(fname="Mim",  lname="Jim")
+
+def function_3(country="Bangladesh"):
+    print("I am from " + country)
+function_3("USA")
+
+name = ["Mim", "Jim", "Kim"]
+def function_4(str):
+    for i in str:
+        print(i)
+function_4(name)
