bc55c609c62326d3ba21915e6003208e41111811
abiryusuf <yusuf_ctg@yahoo.com>
2022-08-20 23:13:58 -0400

    function

---
+++ 13_Function/GeeksforGeeks.py
@@ -0,0 +1,10 @@
+
+num3 = 0
+def add(num1, num2):
+    num3 = num1 + num2
+    return num3
+num1 =5
+num2 =6
+print(add(3, 5))
+ans = add(num1, num2)
+print(f"The addition of {num1} and {num2} results {ans}")
