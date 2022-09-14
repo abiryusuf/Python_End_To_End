feb3d063f217ecc98f09f1c2c55c7de01afae380
abiryusuf <yusuf_ctg@yahoo.com>
2022-09-13 21:55:14 -0400

    update

--- Problem_Solving/String/Reverse_String.py
+++ Problem_Solving/String/Reverse_String.py
@@ -1,45 +1,10 @@
 
-name = "abir" # out put irba
+name = "abir"
 
+def reverseString(ste):
 
-def reverse_string(str):
-
-    res = {}  # store the data
-    for i in str:
-        if i in res:
-            res[i] += 1
-        else:
-            res[i] = 1
+    res = ""
+    for i in ste:
+        res = i + res
     return res
-print(reverse_string("uuuyyyttt"))
-
-# count vowels
-def count_vowels(str):
-    str = str.lower()
-    count = 0
-
-    for i in str:
-        if i in "aieou":
-            count += 1
-    return count
-print(count_vowels("abir"))
-
-count_1 = {i : 0 for i in "aieou"}
-
-for i in "abir".lower():
-    if i in count_1:
-        count_1[i] += 1
-print(count_1)
-
-def palindrom(str):
-    new = ""
-    reverse = ""
-    for i in str.lower():
-        new = new + i
-        reverse = i + reverse
-    if new == reverse:
-        return True
-    else:
-        return False
-print(palindrom("Cadam"))
-
+print(reverseString(name))
