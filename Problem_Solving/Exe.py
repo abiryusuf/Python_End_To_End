06a2e81a01384c4e4ec88357a97d9d07fb4bc6f9
abiryusuf <yusuf_ctg@yahoo.com>
2022-10-05 14:23:47 -0400

    mix, min

--- Problem_Solving/Exe.py
+++ Problem_Solving/Exe.py
@@ -1,12 +1,39 @@
 
-def isPrime(n):
-    if n == 0 or n == 1:
-        return False
-    for i in range(2, n):
-        if n%i == 0:
-            return "It's not prime"
-        else:
-            return "It's prime"
-x = int(input("Enter the number: "))
+num = [2, 4, 4, 5, 15, 5, 6, 6]
 
-print(isPrime(x))
+def sum(n):
+
+    count = 0
+    for i in n:
+        count += i
+    return count
+
+print(sum(num))
+
+def maxNumber(n):
+    max = n[0]
+    min = n[0]
+    secondMax = n[0]
+    thirdMax = n[0]
+    for i in range(len(n)):
+        if n[i] > max:
+            max = n[i]
+        if n[i] < min:
+            min = n[i]
+        if n[i] > secondMax and n[i] != max:
+            secondMax = n[i]
+        if n[i] > thirdMax and n[i] != secondMax and n[i] != max:
+            thirdMax = n[i]
+
+    return max, min, secondMax, thirdMax
+print(maxNumber(num))
+
+def dup(m):
+    m = sorted(m)
+    count = []
+    for i in m:
+        if i not in count:
+            count.append(i)
+    return count
+print(dup(num))
+
