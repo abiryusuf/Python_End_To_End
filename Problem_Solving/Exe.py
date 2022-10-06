1f77b21ffc41c95c01a8ba7d8e709dbde362179a
abiryusuf <yusuf_ctg@yahoo.com>
2022-10-06 16:26:35 -0400

    sort

--- Problem_Solving/Exe.py
+++ Problem_Solving/Exe.py
@@ -1,19 +1,15 @@
 
-num = [2, 4, 4, 5, 15, 5, 6, 6]
+list_A = [1, 3, 10, 9, 11, 60, 5, 16]
+#x = list(input("Enter the Number "))
+def maxNumber(n):
+    max = n[0]
 
-max = num[0]
-min = num[0]
-secMax = num[0]
-print(max)
-for i in range(len(num)):
-    x = num[i]
-    if x > max:
-        max = x
-    if x < min:
-        min = x
-    if x < secMax and x != max:
-        secMax = x
-print("Max number", max, "\n Min number", min)
-print("2nd max ", secMax)
+    for i in range(len(n)):
+        if n[i] > max:
+            max = n[i]
+    return max
+
+print(maxNumber(list_A))
+
 
 
