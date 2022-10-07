a6660ec8f125aa9dd4170e8e5f884f3c4ab0c076
abiryusuf <yusuf_ctg@yahoo.com>
2022-10-07 10:39:57 -0400

    linear search

--- Problem_Solving/Exe.py
+++ Problem_Solving/Exe.py
@@ -1,15 +1,14 @@
 
 list_A = [1, 3, 10, 9, 11, 60, 5, 16]
-#x = list(input("Enter the Number "))
-def maxNumber(n):
-    max = n[0]
+
+def linearSearch(n, target):
 
     for i in range(len(n)):
-        if n[i] > max:
-            max = n[i]
-    return max
-
-print(maxNumber(list_A))
+        x = n[i]
+        if x == target:
+            return True
+    return False
+print(linearSearch(list_A, 11))
 
 
 
